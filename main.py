36dde417732ecc46a431e6ed4efc240667724a3a
schmeddy99 <ahmedaosman00@gmail.com>
2025-03-24 13:04:54 -0700

    Importing csv to table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 
 try:
@@ -44,6 +45,50 @@ try:
 
     """
     )
+
+    # Import CSV to table
+    with open("data/movie_metadata.csv", "r") as movieData:
+        csvreader = csv.reader(movieData)
+        next(csvreader)
+        for row in csvreader:
+
+            cursor.execute(
+                """
+            INSERT INTO MOVIE (
+                color,
+                director_name,
+                num_critic_for_reviews,
+                duration,
+                director_facebook_likes,
+                actor_3_facebook_likes,
+                actor_2_name,
+                actor_1_facebook_likes,
+                gross,
+                genres,
+                actor_1_name,
+                movie_title,
+                num_voted_users,
+                cast_total_facebook_likes,
+                actor_3_name,
+                facenumber_in_poster,
+                plot_keywords,
+                movie_imdb_link,
+                num_user_for_reviews,
+                language,
+                country,
+                content_rating,
+                budget,
+                title_year,
+                actor_2_facebook_likes,
+                imdb_score,
+                aspect_ratio,
+                movie_facebook_likes
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+            """,
+                row,
+            )
+
     # Write a query and execute it with cursor
     query = "SELECT * FROM MOVIE"
     cursor.execute(query)
