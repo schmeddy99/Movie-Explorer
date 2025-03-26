78e71327be1b504dc5964a002ade6e2f9c75eaaa
schmeddy99 <ahmedaosman00@gmail.com>
2025-03-26 07:02:56 -0700

    Adding utility functions to populate director, actor, and genre tables

--- utils.py
+++ utils.py
@@ -14,3 +14,37 @@ def print_results(results):
 
     for row in results:
         print(" | ".join(str(item) for item in row))
+
+
+def get_or_create_director(conn, name, likes):
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM directors WHERE name = ?", (name,))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    cursor.execute(
+        "INSERT INTO directors (name, facebook_likes) VALUES (?, ?)", (name, likes)
+    )
+    return cursor.lastrowid
+
+
+def get_or_create_actor(conn, name, likes):
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM actors WHERE name = ?", (name,))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    cursor.execute(
+        "INSERT INTO actors (name, facebook_likes) VALUES (?, ?)", (name, likes)
+    )
+    return cursor.lastrowid
+
+
+def get_or_create_genre(conn, name):
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM genres WHERE name = ?", (name,))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    cursor.execute("INSERT INTO genres (name) VALUES (?)", (name,))
+    return cursor.lastrowid
